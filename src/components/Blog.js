1c00497edd31e2910fee64171688d2f723b8549d
Theara10 <thearatheng10@gmail.com>
2020-12-16 18:27:05 +0700

    userquery

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -13,45 +13,52 @@ const GET_BLOG = gql`
 `;
 
 export default function Blog() {
+  const { data, loading } = useQuery(GET_BLOG);
+  if (loading) return <div>Loading...</div>;
   return (
-    <div className="blog_container">
-      {/* <Query query={GET_BLOG}>
-        {({ data, loading }) => {
-          if (loading || data === undefined) return <div>data loading...</div>;
-          return (
-            <div>
-              {data.listBlog.map((x) => {
-                return (
-                  <div className="blog_display">
-                    <div className="blog_img">
-                      <Link to="/blog-detail">
-                        <img src={x.image} width="150" height="150" />
-                      </Link>
-                    </div>
-
-                    <div className="blog_text">
-                      <div>
-                        <p className="blog_title">
-                          <Link to="/blog-detail" className="blog_title_link">
-                            {x.title}
-                          </Link>
-                        </p>
-                      </div>
-                      <div>
-                        <p className="blog_sub_title">{x.description}</p>
-                      </div>
-                      <div className="blog_time">
-                        <CalendarTodayIcon fontSize="5" />
-                        <p className="blog_date">Jan 4, 2020</p>
-                      </div>
-                    </div>
-                  </div>
-                );
-              })}
+    <div>
+      {data.listBlog.map((x) => {
+        return (
+          <div className="blog_display">
+            <div className="blog_img">
+              <Link to="/blog-detail">
+                <img src={x.image} width="150" height="150" />
+              </Link>
             </div>
-          );
-        }}
-      </Query> */}
+
+            <div className="blog_text">
+              <div>
+                <p className="blog_title">
+                  <Link to="/blog-detail" className="blog_title_link">
+                    {x.title}
+                  </Link>
+                </p>
+              </div>
+              <div>
+                <p className="blog_sub_title">{x.description}</p>
+              </div>
+              <div className="blog_time">
+                <CalendarTodayIcon fontSize="5" />
+                <p className="blog_date">Jan 4, 2020</p>
+              </div>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
+  // return (
+  //   <div className="blog_container">
+  //     <Query query={GET_BLOG}>
+  //       {({ data, loading }) => {
+  //         if (loading || data === undefined) return <div>data loading...</div>;
+  //         return (
+  //           <div>
+
+  //           </div>
+  //         );
+  //       }}
+  //     </Query>
+  //   </div>
+  // );
 }
