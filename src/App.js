9d492ea4469432a67e898418536246d38f33697d
Theara10 <thearatheng10@gmail.com>
2020-12-29 21:58:28 +0700

    contact mutation

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Dashboard from "./dashboard/Dashboard";
 function App() {
   return (
     <Router>
-      {/* <div className="App">
+      <div className="App">
         <Navbar />
 
         <div className="app_body">
@@ -24,8 +24,8 @@ function App() {
           </Switch>
         </div>
         
-      </div> */}
-      <Dashboard />
+      </div>
+      {/* <Dashboard /> */}
     </Router>
   );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,44 +1,59 @@
 import React from "react";
 import "./Contact.css";
-import { gql } from "apollo-boost";
-import { useMutation } from "@apollo/client";
+import { useMutation, gql } from "@apollo/client";
 
-// const CREATE_CONTACT = gql`
-//   CreateContact($data: ContactInputType){
-//     createContact(data: $data)
-//   }
-// `;
+const CREATE_CONTACT = gql`
+  mutation createContact($data: ContactInputType){
+    createContact(data: $data)
+  }
+`;
 
 function Contact() {
-  let input;
-  // const [createContact, {data}] = useMutation(CREATE_CONTACT)
+  let name;
+  let email;
+  let message;
+  const [createContact] = useMutation(CREATE_CONTACT)
   return (
     <form
       className="contact_container"
       onSubmit={(e) => {
-        // e.preventDefault();
-        // createContact({
-        //   variables: {
-        //     name: input.value,
-        //   },
-        // });
-        // input.value = "";
-      }}
+        e.preventDefault();
+        createContact({
+          variables: {
+           data: {
+            name: name.value,
+            email: email.value,
+            message: message.value
+           }
+          },
+        });
+        name.value = "";
+      }}  
     >
       <h1>Send me a message</h1>
       <div className="contact_input">
         <input
           placeholder="Name"
           ref={(node) => {
-            input = node;
+            name = node;
           }}
         />
       </div>
       <div className="contact_input">
-        <input placeholder="Email" />
+        <input 
+          placeholder="Email"
+          ref={(node) => {
+            email = node;
+          }}
+         />
       </div>
       <div className="contact_input">
-        <textarea placeholder="Message" type="text" />
+        <input 
+          placeholder="Message" 
+          type="text" 
+          ref={(node) => {
+            message = node;
+          }}/>
       </div>
       <div className="contact_button">
         <button>Send</button>
